dead177a7dec70ecefa50f7b28add4f58205191e
Louie Harrison <louieharrison1@hotmail.co.uk>
2020-09-06 16:54:55 +0100

    Add project files

---
+++ CloudflareIP-Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CloudflareIP_Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
