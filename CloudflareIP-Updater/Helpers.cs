80b70a8344596fc8afed556a163b894f1ea70df7
Louie Harrison <louieharrison1@hotmail.co.uk>
2020-09-06 20:10:38 +0100

    Helper to get own public IP

---
+++ CloudflareIP-Updater/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using FunFacts;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CloudflareIP_Updater
+{
+    class Helpers
+    {
+        public static string CurrentPublicIP() 
+        {
+            string url = Program.configuration["myIpUrl"];
+            if (string.IsNullOrEmpty(url))
+                return "";
+            HttpClient client = new HttpClient();
+            try
+            {
+                using (var resp = client.GetAsync(url).Result) 
+                {
+                    return resp.Content.ReadAsStringAsync().Result.Replace("\n", "").Replace(Environment.NewLine, "");
+                }
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
--- CloudflareIP-Updater/Program.cs
+++ CloudflareIP-Updater/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using CloudflareIP_Updater;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace CloudflareIP_Updater
+namespace FunFacts
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IConfigurationRoot configuration;
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(
+              options => options.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning))
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<Worker>()
+                  .Configure<EventLogSettings>(config =>
+                  {
+                      config.LogName = "Cloudflare IP Monitoring Service";
+                      config.SourceName = "CFIP-Monitor";
+                  });
+                // Add access to generic IConfigurationRoot
+                services.AddSingleton<IConfigurationRoot>(configuration);
+            }).UseWindowsService();
+        }
+    }
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private Timer timer;
+        private int interval;
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var freq = Program.configuration["frequency"];
+            this.interval = 1;//TESTING (string.IsNullOrEmpty(freq) ? 2 : int.Parse(freq)) * 1000 * 60;
+            this.timer = new Timer(Callback, null, interval, Timeout.Infinite);
+        }
+        private void Callback(object state)
+        {
+            var currentIP = Helpers.CurrentPublicIP();
+            if (string.IsNullOrEmpty(currentIP))
+                throw new Exception("Unable to fetch IP");
+            //Set next timer
+            timer.Change(interval, Timeout.Infinite);
         }
     }
 }
